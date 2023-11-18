1232441691b40478ae09003c0a1d93f93169f4da
RoidarKhan101 <150510776+RoidarKhan101@users.noreply.github.com>
2023-11-18 06:33:34 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 123456789
+let accountEmail = "Ali@gmail.com"
+var accountPassward = "123456"
+accountCity = "Peshwar"
+let accountState;
+
+accountEmail = "roidarali@nothing"
+accountPassward = "1256854958723"
+accountCity = "karachi"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassward, accountCity, accountState])
