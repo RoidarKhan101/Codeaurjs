90d108dfb4924d314bb1f9fed691c42bb3b3a08f
RoidarKhan101 <150510776+RoidarKhan101@users.noreply.github.com>
2023-12-01 04:39:16 +0000

    datatypes

---
+++ 01_basics/datatypess_summary.js
@@ -0,0 +1,12 @@
+// Primitive
+
+// 7 types : String, Number, Boolean, Null, Undefined, Symbol, BigIngt
+
+// Reference (Non primitive)
+
+// Array, Objects, functions
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+console.log(typeof myfunction);
